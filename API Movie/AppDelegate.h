6a13bb59304cf379a3b8d1161a6e2b3a862cc783
alamin <alamin@alamins-mac.local>
2019-03-09 22:57:20 -0800

    Initial Commit

---
+++ API Movie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  API Movie
+//
+//  Created by Admin on 10/4/18.
+//  Copyright © 2018 alamin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
